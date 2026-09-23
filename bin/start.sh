299b372175096b2f195d13bc28e51729fe48004b
herbertone <130471206+herbertone@users.noreply.github.com>
2026-09-22 18:19:45 -0700

    Delete bin directory

--- bin/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-echo "Collecting static files..."
-python manage.py collectstatic --noinput
-echo "Running migrations..."
-python manage.py migrate
-echo "Starting Gunicorn..."
-exec gunicorn config.wsgi:application --bind 0.0.0.0:$PORT --log-file -
