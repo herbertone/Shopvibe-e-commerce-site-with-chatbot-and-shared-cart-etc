9b049eb16671db29384c17a259d29d675791fe68
herbertone <130471206+herbertone@users.noreply.github.com>
2026-09-22 18:20:34 -0700

    Delete store directory

--- store/utils/supabase_client.py
+++
@@ -1,9 +0,0 @@
-from supabase import create_client, Client
-from django.conf import settings
-
-def get_supabase_client() -> Client:
-    """Initialize and return Supabase client"""
-    if not settings.SUPABASE_URL or not settings.SUPABASE_ANON_KEY:
-        raise ValueError("Supabase URL and Anon Key must be set in environment variables")
-    
-    return create_client(settings.SUPABASE_URL, settings.SUPABASE_ANON_KEY)
